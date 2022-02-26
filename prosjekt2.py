078d5a3d49cadd82f43372ed56dcd0e7236f5cac
Ola Tangen Kulseng <otkulseng1@gmail.com>
2022-02-26 20:30:52 +0100

    viktooooooooooooooooooooor

--- prosjekt2.py
+++ prosjekt2.py
@@ -1,5 +1,23 @@
-def func(a):
-    print(f'Helooooooo {a}')
+import matplotlib.pyplot as plt
+import numpy as np
 
+def victor():
+    def func(x, y):
+        val = 2.8*x**2 * (x**2*(2.5*x**2+y**2-2)+1.2*y**2 * (y*(3*y-0.75)-6.0311)+3.09)+0.98*y**2 * ((y**2-3.01)*y**2+3)-1.005
+        return abs(val) < 0.02
+    n = 3000
+    x = np.linspace(-2, 2, n)
+    y = np.linspace(-2, 2, n)
+    grid = np.zeros((n, n))
+    y, x = np.meshgrid(x, y)
+    z = func(x, y)
+    z = z[:-1, :-1]
+    z_min, z_max = 0, 1
+    fig, ax = plt.subplots()
+    c = ax.pcolormesh(x, y, z, vmin=z_min, vmax=z_max)
+    ax.set_title('Viktooooor')
+    ax.axis([x.min(), x.max(), y.min(), y.max()])
+    fig.colorbar(c, ax=ax)
+    plt.show()
 
-func(23)
+victor()
